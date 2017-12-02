a542c45bbef30f96fc901902d5f0a29dedade4de
sage <laymonage@gmail.com>
2017-12-02 10:06:08 +0700

    Menyiapkan untuk rilis di PyPI

---
+++ setup.py
@@ -0,0 +1,22 @@
+'''
+File setup untuk modul KBBI.
+'''
+
+from setuptools import setup
+setup(
+    name='kbbi',
+    version='0.1',
+    py_modules=['kbbi'],
+    install_requires=[
+        'requests',
+        'beautifulsoup4',
+    ],
+    description=('A Python module that scraps an entry in KBBI'
+                 '(https://kbbi.kemdikbud.go.id)'),
+    author='sage',
+    author_email='laymonage@gmail.com',
+    url='https://github.com/laymonage/kbbi-python',
+    download_url='https://github.com/laymonage/kbbi-python/archive/0.1.tar.gz',
+    keywords=['kbbi', 'kamus', 'indonesia'],
+    classifiers=[],
+)
