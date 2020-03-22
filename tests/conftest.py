f4314ad2022240f85dca5c589786c01ee1910ff2
Sage Muhammad Abdullah <laymonage@gmail.com>
2020-03-22 07:17:14 +0700

    Tambahkan kasus tes untuk fitur khusus pengguna

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,12 +1,23 @@
 import json
+import os
 
 import pytest
 
-from kbbi import KBBI
+from kbbi import KBBI, AutentikasiKBBI
+
+
+@pytest.fixture(scope="session")
+def autentikasi():
+    return AutentikasiKBBI(os.getenv("KBBI_POSEL"), os.getenv("KBBI_SANDI"))
 
 
 @pytest.fixture(scope="session")
 def laman():
+    return dict()
+
+
+@pytest.fixture(scope="session")
+def laman_terautentikasi():
     return dict()
 
 
@@ -24,11 +35,23 @@ def ekspektasi_serialisasi(request):
         return json.load(berkas)
 
 
+def ambil_atau_simpan(dct, key, func):
+    try:
+        return dct[key]
+    except KeyError:
+        dct[key] = func(key)
+        return dct[key]
+
+
 @pytest.fixture
 def aktual_objek(request, laman):
     kueri = request.param
-    try:
-        return laman[kueri]
-    except KeyError:
-        laman[kueri] = KBBI(kueri)
-        return laman[kueri]
+    return ambil_atau_simpan(laman, kueri, lambda a: KBBI(a))
+
+
+@pytest.fixture
+def aktual_objek_terautentikasi(request, autentikasi, laman_terautentikasi):
+    kueri = request.param
+    return ambil_atau_simpan(
+        laman_terautentikasi, kueri, lambda a: KBBI(a, autentikasi)
+    )
